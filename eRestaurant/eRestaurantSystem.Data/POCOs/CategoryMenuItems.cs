fb37b3dc2330cd733f3963021817e8109a77d63d
Exodus Buyser <ebuyser1@studentmail.nait.ca>
2016-11-15 15:52:04 -0700

    Adding CategoryMenuItem POCO

---
+++ eRestaurant/eRestaurantSystem.Data/POCOs/CategoryMenuItems.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eRestaurantSystem.Data.POCOs
+{
+    public class CategoryMenuItems
+    {
+        //this poco will be used by a report
+        //reports need a flat unstructured dataset
+
+        public string CategoryDescription { get; set; }
+        public string ItemDescription { get; set; }
+        public decimal Price { get; set; }
+        public int? Calories { get; set; }
+        public string Comment { get; set; }
+    }
+}
